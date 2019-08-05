c1a129f2307e2b57d80b0e77ac9563a908d969be
rafikkamal <rafik.rkn@gmail.com>
2019-08-06 03:18:09 +0600

    Created a backup for App.vue as installing vue-router wipes out this file. Installed vue-router using 'vue ui'.

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,91 +1,12 @@
 <template>
   <div id="app">
-    <Header headerTitle='Todo List'/>
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
-    <br/>
-    <Header headerTitle='Todo List Using JsonPlaceHolder'/>
-    <AddTodoApi v-on:add-todo-json="addTodoJSON"/>
-    <TodosApi v-bind:todosJSON="todosJSON" v-on:del-todo-json="deleteTodoJSON"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Header from "./components/layout/Header.vue";
-import axios from "axios";
-/**
- * Basic implementation
- */
-import Todos from "./components/Todos.vue";
-import AddTodo from "./components/AddTodo.vue";
-/**
- * New imports for getting data using API
- * JSON Place Holder
- */
-import TodosApi from "./components/JsonPlaceHolder/TodosApi.vue";
-import AddTodoApi from "./components/JsonPlaceHolder/AddTodoApi.vue";
-
-export default {
-  name: 'app',
-  components: {
-    Todos, Header, AddTodo, TodosApi, AddTodoApi
-  },
-  data() {
-    return {
-      todos : [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: true
-        },
-        {
-          id: 4,
-          title: "Todo Four",
-          completed: false
-        }
-      ],
-      todosJSON : []
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-    },
-    deleteTodoJSON(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => this.todosJSON = this.todosJSON.filter(todo => todo.id !== id))
-      .catch(err => console.log(err));
-    },
-    addTodoJSON(newTodo) {
-      /* Destructuring */
-      const { title, completed } = newTodo;
-      axios.post("https://jsonplaceholder.typicode.com/todos", {
-        title, completed
-      })
-      .then(res => this.todosJSON = [...this.todosJSON, res.data])
-      .catch(err => console.log(err));
-    }
-  },
-  created() {
-    axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
-    .then(res => this.todosJSON = res.data)
-    .catch(err => console.log(err));
-  }
-}
-</script>
 
 <style>
   * {
