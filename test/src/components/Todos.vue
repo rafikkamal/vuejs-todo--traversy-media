951f3c99e548d1c07f3fa43f5b8640612a4b35c7
rafikkamal <rafik.rkn@gmail.com>
2019-08-05 17:30:41 +0600

    Created basic Todos.vue

---
+++ test/src/components/Todos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+    <div>
+      <h3 v-bind:key="todo.id" v-for="todo in todos" class="todos">{{todo.title}}</h3>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "Todo",
+    props: ["todos"]
+  }
+</script>
+<style scoped>
+
+</style>
